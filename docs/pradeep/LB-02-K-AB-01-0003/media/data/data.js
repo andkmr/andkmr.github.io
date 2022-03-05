50475285895d7726fa7c50bd4860f556414faf39
andkmr <aanand.yadav@navneet.com>
2022-03-05 10:05:36 +0530

    Adding pardeep's LB-02-K-AB-01-0003

---
+++ docs/pradeep/LB-02-K-AB-01-0003/media/data/data.js
@@ -0,0 +1,96 @@
+var gamedata =
+{
+    "language": "Number",
+    "animationactivity": [
+        {
+            "animaudio": "germs_are_very_small_tiny_a1",
+        },
+        {
+            "animaudio": "they_present_our_hands_a2",
+        },
+        {
+            "animaudio": "germs_can_enter_body_a3",
+        },
+        {
+            "animaudio": "so_we_should_always_take_care_a4",
+        },
+        {
+            "animaudio": "a_clean_body{_healthy_a5",
+        }
+    ],
+    
+    "tabactivity": [
+        {
+            "mascot_audio": "tap_on_the_good_habits",
+            "wrong_mascot_audio": "uh_oh_tap_on_the_good_habits",
+            "allcorrectaudio": "excellent_tapped_all_good_habits",
+            "tabItems": ["gil_sneezing", "girl_bathing", "girl_washing_hands", "dirty_boy", "dirty_room", "oy_cutting_nails"],
+            "tabItemsHabbites": ["yes", "yes", "yes", "no", "no", "yes"],
+            "totalCorrect": 4,
+            "bg": "background_2.png"
+        },
+        {
+            "mascot_audio": "teeth_clean_brushing_everyday",
+            "wrong_mascot_audio": "tap_objects_not_use_clean_teeth",
+            "allcorrectaudio": "fantastic_we_dont_use_clean_teeth",
+            "tabItems": ["floss", "shampoo", "soap", "tooth_brush", "tooth_paste"],
+            "tabItemsHabbites": ["no", "yes", "yes", "no", "no"],
+            "totalCorrect": 2,
+            "bg": "background_3.png"
+        },
+        {
+            "mascot_audio": "tap_odd_objects",
+            "wrong_mascot_audio": "tap_odd_objects",
+            "allcorrectaudio": "superbtappedcorrectly",
+            "tabItems": ["toungue_cleaner", "toothpaste2", "pencil", "napkin", "crayon"],
+            "tabItemsHabbites": ["no", "no", "yes", "no", "yes"],
+            "totalCorrect": 2,
+            "bg": "background_4.png"
+        }
+        
+    ],
+    "gamedataQues": [
+        {
+            "dragItems": ["boy_eating_icecream", "boy_playing_with_toys", "boy_reading"],
+            "dropItems": ["boy_eating_icecream_drop", "boy_playing_with_toys_drop", "boy_reading_drop"],
+            "bg": "background_5.png",
+            "drag_type": "drag_level1",
+            "drop_type": "drop_level1",
+            "mascot_audio": "drag_drop_images",
+            "mascot_audio_try": "1a",
+            "audio": [
+            ]
+        },{
+            "dragItems": ["boycooking", "girlsleeping", "skippinggirl"],
+            "dropItems": ["boycooking_drop", "girlsleeping_drop", "skippinggirl_drop"],
+            "bg": "background_5.png",
+            "drag_type": "drag_level2",
+            "drop_type": "drop_level2",
+            "mascot_audio": "drag_drop_images",
+            "mascot_audio_try": "1a",
+            "audio": [
+            ]
+        },
+        {
+            "dragItems": ["boyjumping", "girlhopping", "gilonslide"],
+            "dropItems": ["boyjumping_drop", "girlhopping_drop", "gilonslide_drop"],
+            "bg": "background_5.png",
+            "drag_type": "drag_level3",
+            "drop_type": "drop_level3",
+            "mascot_audio": 1,
+            "mascot_audio_try": "1a",
+            "audio": [
+            ]
+        },{
+            "dragItems": ["swinginggirl", "girlwithfamily", "girlswiming"],
+            "dropItems": ["swinginggirl_drop", "girlwithfamily_drop", "girlswiming_drop"],
+            "bg": "background_5.png",
+            "drag_type": "drag_level4",
+            "drop_type": "drop_level4",
+            "mascot_audio": 1,
+            "mascot_audio_try": "1a",
+            "audio": [
+            ]
+        }
+    ]
+};
