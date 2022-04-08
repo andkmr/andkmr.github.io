b4626b1d9d7e62af1b559a2e255bd63d642fa361
andkmr <aanand.yadav@navneet.com>
2022-04-08 12:04:01 +0530

    Adding webgl test game

--- docs/webgl/__LB-02-M-SH-18-0003/js/bridge.js
+++
@@ -1,9 +0,0 @@
-function receiveMessageFromUnity(urlData) {
-
-   window.open(urlData, "_self");
-
-}
-
-function sendMessageToUnity(message) {
-   unityGameInstance.SendMessage("[Bridge]", "ReceiveMessageFromPage", message);
-}
